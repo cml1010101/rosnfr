2a065fb0c8de6650031763bb8570dd49b2541f12
cml1010101 <cml1010101@gmail.com>
2023-05-19 20:52:20 -0400

    adjusted the way that the daemon is configured

--- scripts/install-aarch64.sh
+++ scripts/install-aarch64.sh
@@ -7,9 +7,16 @@ sudo apt install -y nvidia-jetpack
 sudo systemctl restart docker
 sudo usermod -aG docker $USER
 newgrp docker
-sudo apt install -y curl
-curl https://raw.githubusercontent.com/cml1010101/rosnfr/main/resources/arm64-daemon.json > arm64-daemon.json
-sudo rm -rf /etc/docker/daemon.json
-sudo mv arm64-daemon.json /etc/docker/daemon.json
+sudo tee /etc/docker/daemon.json <<EOF
+{
+    "runtimes": {
+        "nvidia": {
+            "path": "/usr/bin/nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}
+EOF
+sudo pkill -SIGHUP dockerd
 sudo systemctl daemon-reload && sudo systemctl restart docker
 scripts/install.sh
--- scripts/install-x86_64.sh
+++ scripts/install-x86_64.sh
@@ -11,9 +11,16 @@ curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidi
 sudo apt update
 sudo apt install -y nvidia-container-runtime
 sudo apt install -y nvidia-docker2
-curl https://raw.githubusercontent.com/cml1010101/rosnfr/main/resources/x86_64-daemon.json > x86_64-daemon.json
-sudo rm -rf /etc/docker/daemon.json
-sudo mv x86_64-daemon.json /etc/docker/daemon.json
+sudo tee /etc/docker/daemon.json <<EOF
+{
+    "runtimes": {
+        "nvidia": {
+            "path": "/usr/bin/nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}
+EOF
 sudo pkill -SIGHUP dockerd
 sudo systemctl daemon-reload && sudo systemctl restart docker
 scripts/install.sh
