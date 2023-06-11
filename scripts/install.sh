a6897ed9f8c36beb7ef7816d99d6ba9dd514c55f
cml1010101 <cml1010101@gmail.com>
2023-06-11 10:02:34 -0400

    fixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,6 @@ echo "export ISAAC_ROS_WS=$(pwd)" >> ~/.bashrc
 source ~/.bashrc
 git submodule update --init --recursive
 docker login nvcr.io $oauthtoken
-touch src/isaac_ros_common/scripts/.isaac_ros_common-config
-echo "CONFIG_IMAGE_KEY=ros2_humble.realsense.wpilib" >> src/isaac_ros_common/scripts.isaac_ros_common-config
 echo $ISAAC_ROS_WS
 #cd src/isaac_ros_common && \
 #    ./scripts/run_dev.sh $ISAAC_ROS_WS
