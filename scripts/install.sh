61a4d36b38ed73a6ec7f5ed5ade0a5ab6e22ee1f
cml1010101 <cml1010101@gmail.com>
2023-06-11 10:01:33 -0400

    made install pull cml1010101/rosnfr:v8 from hub.docker.io

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,9 +6,9 @@ source ~/.bashrc
 git submodule update --init --recursive
 docker login nvcr.io $oauthtoken
 touch src/isaac_ros_common/scripts/.isaac_ros_common-config
-echo "CONFIG_IMAGE_KEY=ros2_humble.realsense" >> src/isaac_ros_common/scripts.isaac_ros_common-config
+echo "CONFIG_IMAGE_KEY=ros2_humble.realsense.wpilib" >> src/isaac_ros_common/scripts.isaac_ros_common-config
 echo $ISAAC_ROS_WS
 #cd src/isaac_ros_common && \
 #    ./scripts/run_dev.sh $ISAAC_ROS_WS
-docker pull cml1010101/rosnfr:v5
+docker pull cml1010101/rosnfr:v8
 exit 0
